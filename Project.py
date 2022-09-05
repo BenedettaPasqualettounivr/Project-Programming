61b335ebb89b2eca8da81c752f4a8eeb4de69a96
BenedettaPasqualettounivr <you@example.com>
2022-09-05 12:03:13 +0200

    5/09

--- Project.py
+++ Project.py
@@ -44,30 +44,39 @@ st.text("Hence we would be removing all the data pertaining to these years from 
 st.text("creating a new dataset")
 st.code('''spotify_df_years_drop = spotify_df[(spotify_df['year'] <2000) | (spotify_df['year'] > 2019)].index
 df = spotify_df.drop(spotify_df_years_drop)''')
-spotify_df_years_drop = spotify_df[(spotify_df['year'] <2000) | (spotify_df['year'] > 2019)].index
-df = spotify_df.drop(spotify_df_years_drop)
+
 
 st.subheader("Then we start to analyse some interesting variables:")
-st.text("ARTIST")
+st.subheader("ARTIST")
 st.text("First of all we check how many songs each artist did:")
 st.code('''artist=df['artist'].value_counts()''')
-artist = spotify_df['artist']
-print(artist)
+
 st.text("Then we plot it, in order to see which are the five most important artists:")
 st.text("So, the most popular artist is Rihanna, since")
 st.code('''pop_artist = df.groupby('artist')[['artist','popularity']].sum().sort_values('popularity',ascending=False).head(10)
 print('The most popular artist is', pop_artist.index[0])''')
-tp_artists_songs= artist[:5]
-tp_artists_name =artist[:5]
-fig = plt.figure(figsize = (10, 5))
-plt.bar(tp_artists_name,tp_artists_songs,width = 0.4,color="forestgreen")
-plt.xlabel("Artists")
-plt.ylabel("No of Songs")
-plt.title('Top Artists with Hit Songs',color = 'black',fontsize = 20)
-plt.show(tp_artists_name, tp_artists_songs)
-st.bar_chart(tp_artists_songs, tp_artists_name)
 
-st.text("DURATION")
+
+st.subheader("GENRE")
+st.text("Now we analize the variable Genre. We count how many songs there are for each genre.")
+st.code('''genre = spotify_df['genre'].value_counts()''')
+st.text("In order to better visualize this we made this barchart.")
+st.text("BAR CHART")
+st.text("Here we can see that the most populated genre is pop.")
+st.text("There are a lot of genres so we decide to focus on the first 10 most popular.")
+st.code('''genre_distribution = pd.DataFrame(spotify_df.genre.value_counts().rename_axis('Genre').reset_index(
+  name='total'))
+genre_distribution.head(10)''')
+st.text("So we made a pie chart to see their distribution in percentage.")
+st.text("Focusing then on the Hits, we can now see the Top 5 Genres which are hits.")
+st.code('''tp_genres=genre[:5]
+tp_genres_names=genre[:5].index''')
+st.text("BAR CHART")
+st.text("Pop seems to be the most popular type of genre. In fact, 428 songs of the")
+st.text("top-hits spotify songs since 2000-2019 belong to pop. This is followed by")
+st.text("hip-hop,pop which is the 2nd most popular conbination of genre.")
+
+st.subheader("DURATION")
 st.text("Since we want to check the average Duration of Top Hit Songs for each year, we start")
 st.text("from the conversion the duration of songs from milliseconds to minutes and seconds.")
 st.text("Then we find out the five longest songs")
@@ -77,23 +86,69 @@ st.text("And the five shortest songs")
 st.code('''songs_by_duration[-5:]''')
 st.text("Then we analyse the trend during the year of the duration of songs.")
 st.text("As we can see here, the duration has a decreasing trend.")
-def time_convert(ms):
-    sec=ms/1000
-    return f"{int(sec//60)}:{int(sec%60)}"
-durations = df[['duration_ms','year']].groupby('year').mean().reset_index().iloc[0:20]
-durations['min:sec'] = durations['duration_ms'].apply(time_convert)
-durations
-x=df["year"].unique()
-x=sorted(x)
-y=(durations["min:sec"])
-x=pd.Series(x)
-plt.figure(figsize=(12,8))
-sns.lineplot(x,y,data=df)
-plt.title('Change in the Duration of Song',fontsize=15)
-plt.xlabel('Years',size=10)
-plt.ylabel('Time',size=10)
-plt.show()
 
 st.text("Then we search the number of songs per year, as we can see here there")
 st.text("isn't a specific trend")
 
+st.subheader("EXPLICIT CONTENT")
+st.text("Now we want to have a view of the amount of explicit content in the top-hits.")
+st.text("We firstly count the amount of explicit and not-explicit content and then we plot it with a Barchart")
+st.text("and with a piechart.")
+st.code('''data=spotify_df['explicit'].value_counts()
+explicit_songs_or_not = (spotify_df.explicit.value_counts().rename_axis('Explicit').reset_index(name = 'songs'))
+explicit_songs_or_not''')
+st.text("BARCHART")
+st.text("Here we used the piechart in order to have the percentage")
+st.text("PIECHART")
+st.text("Now, we visualize the number of top-hits wich are explicit over the years. ")
+st.code('''song_yr_explicit = spotify_df.groupby(['year','explicit']).size().unstack(fill_value=0).reset_index()
+song_yr_explicit.rename(columns={False:'Clean', True: 'Explicit'}, inplace=True)''')
+st.text("BARCHART")
+st.text("Here we can see another way to visualize the amount of explicit and non explicit content by a different")
+st.text("barchart.")
+st.text("BARCHART")
+st.text("BARCHART")
+st.text("Here we can see another way to visualize the amount of explicit and non explicit content by a different")
+st.text("barchart.")
+st.text("BARCHART")
+
+st.subheader("DANCEABILITY")
+st.text("Another interesting varibale is deanceability, as we can see by this graph:")
+st.text("There is a correlation between Danceability and density.")
+
+st.subheader("CORRELATION")
+st.text("Now we will analyze the correlation between various features. To do this we create a mew dataframe to find")
+st.text("the relation among the numerical variables present in the dataset. ")
+st.code('''df_new = spotify_df[['duration_ms', 'year', 'popularity', 'danceability', 'energy', 
+     'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']]''')
+st.text("The result of the study is the following heatmap where we plot all the correlations between the variables.")
+st.text("HEATMAP")
+
+st.subheader("FOCUS ON RIHANNA")
+st.text("Since the most popular artist is Rihanna, we decide to do a focus on her.")
+st.text("We filter the dataset in order to obtain only the songs of Rihanna.")
+st.code("rihanna_df = spotify_df[spotify_df['artist'] == 'Rihanna']")
+st.text("We count then the number of songs she has.")
+st.code('''len(rihanna_df.song.unique())''')
+st.text("We now use a code to get the popularity and the number of songs per year.")
+st.text("BARCHART")
+st.text("Then in order to have an overview of Rihanna we calculate the sum of her")
+st.text("popularity:")
+
+st.text("Then we proceed calculating her five best songs:")
+
+st.text("And, to conclude, if there are correlations between the variables considering only her songs")
+
+st.header("CLUSTERING")
+st.text("We decide to do a clustering in order to see if there is a correlation")
+st.text("between some varibales. In this case betweeen energy and loudness.")
+st.text("First of all, through the Elbow method, we find out the best numer of clusters")
+
+st.text("Then we divide our data in clusters")
+st.code('''df.insert(0, 'Cluster', clusters, True)''')
+st.text("So we plot our data divided in five clusters")
+
+st.text("We can see that for each genre there are specific values for energy and")
+st.text("loudness: for instance pop has an high value both of energy and loudness.")
+st.text("The trend is deacreasing since the other genrs have progressively less energy")
+st.text("and loudness. At the end of the scale we find out R&B.")
